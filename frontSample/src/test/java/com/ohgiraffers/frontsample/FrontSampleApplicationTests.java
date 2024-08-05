c0b1eb4150f691a59f06965aa3f1cf3ee67c9dc3
min-g126 <qoalstj01@gmail.com>
2024-08-06 01:39:51 +0900

    서퍼톤 대비 부트스트랩 공부

---
+++ frontSample/src/test/java/com/ohgiraffers/frontsample/FrontSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.frontsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
