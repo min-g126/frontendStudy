5e97271e2600639524d52051c09712cb97a76473
min-g126 <qoalstj01@gmail.com>
2024-08-11 23:44:56 +0900

    test controller 추가

--- frontSample/src/main/java/com/ohgiraffers/frontsample/controller/MainController.java
+++ frontSample/src/main/java/com/ohgiraffers/frontsample/controller/MainController.java
@@ -13,5 +13,5 @@ public class MainController {
         return "main";
     }
 
-    
+
 }
---
+++ frontSample/src/main/java/com/ohgiraffers/frontsample/controller/TestController.java
@@ -0,0 +1,5 @@
+package com.ohgiraffers.frontsample.controller;
+
+public class TestController {
+    /* 부트스트랩 메인 컨트롤*/
+}
